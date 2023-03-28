b640506944d4286364cddc176583b2cc748df534
cleber geraldo <89355850+cleber77@users.noreply.github.com>
2023-03-28 20:12:19 -0300

    MyBanck

--- app/src/main/java/com/Cleber/meubanco/MainActivity.java
+++ app/src/main/java/com/Cleber/meubanco/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,13 +25,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-
                 Intent navegatela = new Intent(MainActivity.this, Saldo.class);
                 startActivity(navegatela);
-
-
             }
         });
+        binding.imgFatura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent navegarTelaDeFatura = new Intent(MainActivity.this,Fatura.class);
+                startActivity(navegarTelaDeFatura);
+                finish();
+            }
+        });
+
+
     }
 }
 
